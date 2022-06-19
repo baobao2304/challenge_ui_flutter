e8499dca238967e1340e658c1fd8c2fcd6745fb2
ADMIN\Admin <lebao230499@gmail.com>
2022-06-19 14:46:13 +0700

    add source flutter

---
+++ android/app/src/main/kotlin/com/baopham/challengeui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.baopham.challengeui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>  with SingleTickerProviderStateMixin{
+  late TabController _tabController;
+
+  List<Tab> tabs = <Tab>[
+    const Tab(text: 'Car',icon: Icon(Icons.car_rental)),
+    const Tab(text: 'Book',icon: Icon(Icons.book)),
+    const Tab(text: 'Computer',icon: Icon(Icons.laptop)),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: tabs.length);
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Readerstacks Top Tab'),
+            ),
+
+            body:Column(
+
+              children: [ TabBar(
+                // labelStyle: const TextStyle(fontWeight: FontWeight.bold),
+                // automaticIndicatorColorAdjustment: true,
+                indicatorColor: Colors.red,
+                labelColor: Colors.red,
+                controller: _tabController,
+                tabs: tabs,
+              ),
+                Expanded(child:TabBarView(
+
+                  controller: _tabController,
+                  children: tabs.map((Tab tab) {
+                    return Center(
+                      child: Text(
+                        '${tab.text!} Tab',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    );
+                  }).toList(),
+                ))],
+            )
+        )
+    );
+
+  }
+}
